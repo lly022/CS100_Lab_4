df0e05df946828effd9da266043b3496a28ba18d
lly022 <lly022@ucr.edu>
2020-11-03 18:10:22 -0800

    Add more sub class unit tests

--- sub_test.hpp
+++ sub_test.hpp
@@ -5,19 +5,62 @@
 
 #include "sub.hpp"
 
-TEST(SubTest, SubEvaluateNormalValues){
-        Op* op1 = new Op(8);
-        Op* op2 = new Op(4);
-        Sub* test  = new Sub(op1, op2);
-        EXPECT_EQ(test->evaluate(), 4);
+TEST(SubTest, NumSubNumEvaluate){
+	Op* eight = new Op(8);
+	Op* four = new Op(4);
+	Sub* test = new Sub(eight, four);
+	EXPECT_EQ(test->evaluate(), 4);
 }
 
-TEST(SubTest, SubStringifyNormalValues){
-        Op* op1 = new Op(8);
-        Op* op2 = new Op(4);
-        Sub* test  = new Sub(op1, op2);
-        EXPECT_EQ(test->stringify(), "8.000000-4.000000");
+TEST(SubTest, NumSubNumStringify){
+	Op* eight = new Op(8);
+	Op* four = new Op(4);
+	Sub* test = new Sub(eight, four);
+	EXPECT_EQ(test->stringify(), "8.000000-4.000000");
 }
+
+TEST(SubTest, ZeroSubNumEvaluate){
+	Op* three = new Op(3);
+	Op* zero = new Op(0);
+	Sub* test = new Sub(zero, three);
+	EXPECT_EQ(test->evaluate(), -3);
+}
+
+TEST(SubTest, ZeroSubNumStringify){
+        Op* three = new Op(3);
+        Op* zero = new Op(0);
+        Sub* test = new Sub(zero, three);
+        EXPECT_EQ(test->stringify(), "0.000000-3.000000");
+}
+
+TEST(SubTest, NegNumSubNumEvaluate){
+        Op* negThree = new Op(-3);
+        Op* five = new Op(5);
+        Sub* test = new Sub(negThree, five);
+        EXPECT_EQ(test->evaluate(), -8);
+}
+
+TEST(SubTest, NegNumSubNumStringify){
+	Op* negThree = new Op(-3);
+	Op* five = new Op(5);
+	Sub* test = new Sub(negThree, five);
+	EXPECT_EQ(test->stringify(), "-3.000000-5.000000");
+}
+
+TEST(SubTest, NegNumSubNegNumEvaluate){
+	Op* negThree = new Op(-3);
+	Op* negFive = new Op(-5);
+	Sub* test = new Sub(negThree, negFive);
+	EXPECT_EQ(test->evaluate(), 2);
+}
+
+TEST(SubTest, NegNumSubNegNumStringify){
+	Op* negThree = new Op(-3);
+	Op* negFive = new Op(-5);
+	Sub* test = new Sub(negThree, negFive);
+	EXPECT_EQ(test->stringify(), "-3.000000--5.000000");
+}
+
 
 
 
