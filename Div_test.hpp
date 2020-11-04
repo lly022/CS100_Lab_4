cea7cc4785cbcd2d81c9fc75e52d8182d1487907
lly022 <lly022@ucr.edu>
2020-11-03 21:57:44 -0800

    Add more Div class unit tests

--- Div_test.hpp
+++ Div_test.hpp
@@ -62,6 +62,36 @@ TEST(DivTest, DivTwoNegStringify){
         EXPECT_EQ(test->stringify(), "-8.000000/-4.000000");
 }
 
+TEST(DivTest, DecDividedByDecEvaluate){
+	Op* op1 = new Op(2.2222);
+	Op* op2 = new Op(-1.1111);
+	Div* test = new Div(op1, op2);
+	EXPECT_EQ(test->evaluate(), -2);
+}
+
+TEST(DivTest, DecDividedByDecStringify){
+	Op* op1 = new Op(2.2222);
+	Op* op2 = new Op(-1.1111);
+	Div* test = new Div(op1, op2);
+	EXPECT_EQ(test->stringify(), "2.222200/-1.111100");
+}
+
+TEST(DivTest, AddDividedByNumEvaluate){
+	Base* four = new Op(4);
+	Base* six = new Op(6);
+	Base* two = new Op(2);
+	Add* ten = new Add(four, six);
+	Div* test = new Div(ten, two);
+	EXPECT_EQ(test->evaluate(), 5);
+}
+
+TEST(DivTest, AddDividedByNumStringify){
+        Base* four = new Op(4);                                                                                                 Base* six = new Op(6);
+        Base* two = new Op(2);
+        Add* ten = new Add(four, six);
+        Div* test = new Div(ten, two);
+        EXPECT_EQ(test->stringify(), "4.000000 + 6.000000/2.000000");
+}
 
 
 #endif //__DIV_TEST_HPP__
