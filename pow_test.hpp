feea0046fef996bfa371a7e2e5312c84547c4d6f
kevin7816 <kevinliang7816@gmail.com>
2020-11-03 20:37:08 -0800

    Added more testcases

--- pow_test.hpp
+++ pow_test.hpp
@@ -14,4 +14,34 @@ TEST(PowTest, NumToZero) {
     EXPECT_EQ(test->evaluate(), 1);
 }
 
+TEST(PowTest, NumToNum) {
+    Op* two = new Op(2);
+    Op* eight = new Op(8);
+    Pow* test = new Pow(eight,two);
+    EXPECT_EQ(test->evaluate(), 64);
+}
+
+TEST(PowTest, NegNumToNum) {
+    Op* two = new Op(-2);
+    Op* three = new Op(3);
+    Pow* test = new Pow(two,three);
+    EXPECT_EQ(test->evaluate(), -8);
+}
+
+TEST(PowTest, NumToNegNum) {
+    Op* two = new Op(-2);
+    Op* eight = new Op(8);
+    Pow* test = new Pow(eight,two);
+    EXPECT_EQ(test->evaluate(), (1/64));
+}
+
+TEST(PowTest, StringNumToNum) {
+    Op* two = new Op(2);
+    Op* eight = new Op(8);
+    Pow* test = new Pow(eight,two);
+    EXPECT_EQ(test->stringify(), "8.000000 ** 2.000000");
+}
+
+
+
 #endif
