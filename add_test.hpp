6f2ce950a52d70082d6252f034382b4f0908be0e
lly022 <lly022@ucr.edu>
2020-11-03 21:42:23 -0800

    Add more unit tests to add_test.hpp

--- add_test.hpp
+++ add_test.hpp
@@ -1,10 +1,11 @@
-#ifndef __MULT_TEST_HPP__
-#define __MULT_TEST_HPP__
+#ifndef __ADD_TEST_HPP__
+#define __ADD_TEST_HPP__
 
 #include "gtest/gtest.h"
 
 #include "add.hpp"
 #include "op.hpp"
+#include "mult.hpp"
 
 TEST(AddTest, NumPlusZero) {
     Op* zero = new Op(0);
@@ -55,7 +56,37 @@ TEST(AddTest, StringNegNumPlusNegNum) {
     EXPECT_EQ(test->stringify(), "-1.000000 + -8.000000");
 }
 
+TEST(AddTest, DecPlusNegDecEvaluate){
+    Op* op1 = new Op(1.555);
+    Op* op2 = new Op(-0.554); 
+    Add* test = new Add(op1, op2);
+    EXPECT_EQ(test->evaluate(), 1.001);
+}
 
+TEST(AddTest, DecPlusNegDecStringify){
+    Op* op1 = new Op(1.555);
+    Op* op2 = new Op(-0.554);
+    Add* test = new Add(op1, op2);
+    EXPECT_EQ(test->stringify(), "1.555000 + -0.554000");
+}
+
+TEST(AddTest, MultPlusNumEvaluate){
+   Base* four = new Op(4);
+   Base* seven = new Op(7);
+   Base* three = new Op(3);
+   Mult* twentyEight = new Mult(seven, four);
+   Add* test = new Add(three, twentyEight);
+   EXPECT_EQ(test->evaluate(), 31);  
+}
+
+TEST(AddTest, MultPlusNumStringify){
+   Base* four = new Op(4);
+   Base* seven = new Op(7);
+   Base* three = new Op(3);
+   Mult* twentyEight = new Mult(seven, four);
+   Add* test = new Add(three, twentyEight);
+   EXPECT_EQ(test->stringify(), "3.000000 + 7.000000 * 4.000000");
+}
 
 
 #endif
